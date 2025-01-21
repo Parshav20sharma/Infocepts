22e7820e647b29a7aca0498021e743fed302a68e
Parshav Sharma <130817725+Parshav20sharma@users.noreply.github.com>
2025-01-21 12:47:20 +0530

    Your commit message here

--- local_rag_agent.py
+++ local_rag_agent.py
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 from phi.agent import Agent
 from phi.model.ollama import Ollama
 from phi.knowledge.pdf import PDFUrlKnowledgeBase
 from phi.vectordb.qdrant import Qdrant
 from phi.embedder.ollama import OllamaEmbedder
 from phi.playground import Playground, server_playground_app
+=======
+from phi.agent import Agent
+from phi.model.ollama import Ollama
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.vectordb.qdrant import Qdrant
+from phi.embedder.ollama import OllamaEmbedder
+from phi.playground import Playground, server_playground_app
+from
+>>>>>>> 88e5170 (Uploading Project)
