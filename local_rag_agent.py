47274ab4c9d6a61d4c5ec1618a0296a4fedf9bd2
Parshav Sharma <130817725+Parshav20sharma@users.noreply.github.com>
2025-01-21 12:26:30 +0530

    Add files via upload

---
+++ local_rag_agent.py
@@ -0,0 +1,6 @@
+from phi.agent import Agent
+from phi.model.ollama import Ollama
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.vectordb.qdrant import Qdrant
+from phi.embedder.ollama import OllamaEmbedder
+from phi.playground import Playground, server_playground_app
